a16da84c8c0f3129e1ae4a6380b3bcdc8e9efded
harsh <hg@gatech.edu>
2018-06-15 15:33:11 -0400

    added contrib auth authentication

--- server/models.py
+++ server/models.py
@@ -1,21 +1,24 @@
 import uuid
 from django.db import models
+from django.contrib.auth.base_user import AbstractBaseUser
 
-class User(models.Model):
+class User(AbstractBaseUser):
 
 	id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False) 
 	created_at = models.DateTimeField(auto_now_add=True)
 	updated_at = models.DateTimeField(auto_now=True)
 	email = models.EmailField(max_length=255,blank=False,unique=True,null=False)
-	password = models.CharField(max_length=255,null=False,blank=False)
 	verified = models.BooleanField(default=False,blank=False)
 	pub_key = models.TextField(max_length=1024,blank=False,unique=True,null=False)
 	size_used = models.IntegerField(default=0, blank=False,null=False)
 	size_limit = models.IntegerField(default=500000, blank=False,null=False)
 	s3_bucket = models.CharField(max_length=255,default='central')
 	s3_bucket_key = models.CharField(max_length=255,null=False,unique=True)
+	
+	USERNAME_FIELD = 'email'
+	
 
-
+	
 class Bucket(models.Model):
 
 	id = models.UUIDField(primary_key=True,default=uuid.uuid4,editable=False)
