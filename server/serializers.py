3811b8ce6398b829385c5f296a84c4cf14e48880
harsh <hg@gatech.edu>
2018-06-16 14:59:02 -0400

    removed autoserialization

--- server/serializers.py
+++
@@ -1,7 +0,0 @@
-from .models import IndividualEncryptionKey
-from rest_framework import serializers
-
-class IndividualEncryptionKeySerializer(serializers.ModelSerializer):
-    class Meta:
-        model = IndividualEncryptionKey
-        fields = ('encrypte_key', 'created_by', 'created_for')
--- server/views.py
+++ server/views.py
@@ -1,8 +1,3 @@
-from .models import Bucket
-from rest_framework.views import APIView
-
-class BucketList(APIView):
-
-	def get(self,request,format=None):
-		buckets = Bucket.objects.query.all(owner_id=)
+# from .models import Bucket
+# from rest_framework.views import APIView
 
